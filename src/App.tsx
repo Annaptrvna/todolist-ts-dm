afe75524ff0e31303cde1e9f1ca059573e1f35b2
Anna <annagjindj@gmail.com>
2024-05-22 06:54:20 -0400

    Add Todolist component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-
+      <Todolist/>
+      <Todolist/>
+      <Todolist/>
     </div>
   );
 }
 
+const Todolist = () =>{
+
+    return (
+        <div>
+            <h3>What to learn</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li>
+                    <input type="checkbox"/>
+                    <span>HTML</span>
+                </li>
+                <li>
+                    <input type="checkbox"/>
+                    <span>CSS</span>
+                </li>
+                <li>
+                    <input type="checkbox"/>
+                    <span>JS</span>
+                </li>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </ul>
+        </div>
+    )
+}
 export default App;
